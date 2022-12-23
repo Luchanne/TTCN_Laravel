0a5d2a68fc5cc6256ad27a1a40a656c49c89f469
Luchan <116260289+Luchanne@users.noreply.github.com>
2022-12-23 16:08:09 +0700

    Tuần 3,4,5 (hoàn thành)
    Web bán hàng sử dụng Laravel

---
+++ quanlybanhang/config/order.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'status' => [
+        'chờ duyệt' => 'chờ duyệt',
+        'đã duyệt' => 'đã duyệt',
+        'đang vận chuyển' => 'đang vận chuyển',
+        'giao hàng thành công' => 'giao hàng thành công',
+        'hủy đơn' => 'hủy đơn'
+    ]
+];
